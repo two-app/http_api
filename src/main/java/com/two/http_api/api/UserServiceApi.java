171debd44fc8c4203cdc317a871daf52b955d51a
gerryfletch <1997Gerry@gmail.com>
2019-09-01 21:37:56 +0100

    Created basic user service API and tested it with auth service

--- src/main/java/com/two/http_api/api/UserServiceApi.java
+++ src/main/java/com/two/http_api/api/UserServiceApi.java
@@ -1,19 +1,33 @@
 package com.two.http_api.api;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import com.two.http_api.model.User;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class UserServiceApi {
 
-    private final String userServiceUrl = "http://localhost:8084";
+    private final EurekaClient client;
+
+    public UserServiceApi(@Qualifier("eurekaClient") EurekaClient client) {
+        this.client = client;
+    }
 
     public ResponseEntity<User> getUser(int uid) {
-        return new RestTemplate().getForEntity(userServiceUrl + "?uid=" + uid, User.class);
+        return new RestTemplate().getForEntity(getUserServiceUrl() + "?uid=" + uid, User.class);
     }
 
     public ResponseEntity<User> getUser(String email) {
-        return new RestTemplate().getForEntity(userServiceUrl + "?email=" + email, User.class);
+        return new RestTemplate().getForEntity(getUserServiceUrl() + "?email=" + email, User.class);
+    }
+
+    private String getUserServiceUrl() {
+        String url = client.getNextServerFromEureka("service-users", false).getHomePageUrl();
+        return url + "user";
     }
 
 }
