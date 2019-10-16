9389e6ccddaa1219aa8d00a17f1f910d2e7c7bd1
gerryfletch <1997Gerry@gmail.com>
2019-10-16 10:34:23 +0100

    Created an API and models for the public endpoints.

---
+++ src/main/java/com/two/http_api/api/PublicApiContracts.java
@@ -0,0 +1,38 @@
+package com.two.http_api.api;
+
+import com.two.http_api.model.Tokens;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import static com.two.http_api.model.PublicApiModel.UserLogin;
+import static com.two.http_api.model.PublicApiModel.UserRegistration;
+
+public class PublicApiContracts {
+
+    public interface PostLogin {
+        String loginPostMapping = "/login";
+
+        @PostMapping(loginPostMapping)
+        Tokens login(@RequestBody @Valid UserLogin userLogin);
+    }
+
+    public interface PostConnect {
+        String postConnectPath = "/connect/{connectCode}";
+        String connectCodePathVariable = "connectCode";
+
+        @PostMapping(postConnectPath)
+        Tokens connect(HttpServletRequest request, @PathVariable(connectCodePathVariable) String partnerConnectCode);
+    }
+
+    public interface PostSelf {
+        String postSelfPath = "/self";
+
+        @PostMapping(postSelfPath)
+        Tokens registerUser(@RequestBody @Valid UserRegistration user);
+    }
+
+}
