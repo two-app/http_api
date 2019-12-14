e785b2f319e57f589d7929bb49c5774d7aca0aad
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2019-12-14 20:54:07 +0000

    Removed unused field (#8)

--- src/main/java/com/two/http_api/model/User.java
+++ src/main/java/com/two/http_api/model/User.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.experimental.Wither;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -28,12 +26,13 @@ public class User {
     @Email(message = "You must provide a valid email.")
     private String email;
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @Age(13)
-    private LocalDate dob;
+    @NotEmpty(message = "First Name must be provided.")
+    @Length(min = 2, message = "First Name must be at least 2 characters long.")
+    private String firstName;
 
-    @NotEmpty(message = "You must provide a name.")
-    private String name;
+    @NotEmpty(message = "Last Name must be provided.")
+    @Length(min = 2, message = "Last Name must be at least 2 characters long.")
+    private String lastName;
 
     @Data
     @AllArgsConstructor
